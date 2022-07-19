535be881511f19d5e72d802cfa8774bf42f172e6
Farinmade <58787138+erinayo30@users.noreply.github.com>
2022-07-19 16:03:19 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//1.API Url
+const Url * "https://jsonplaceholder.typicode.com/users";
+
+//2. fetch users from the API url
+function fetchusers{}<
+//make use of the brower fetch API
+fetch (Url).then 
+>
+//3. Render the users in the DOM
+
+//4. Add a search function to the DOM
+let str = "please locate where 'username' occurs";
+str.search("username")
